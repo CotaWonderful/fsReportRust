d876e3164c8c4de0de681c2819246de74fbcb6ba
wonderful <wonderful@cotabank.com.tw>
2025-06-05 09:31:14 +0800

    Remove redundent comment

--- src/main.rs
+++ src/main.rs
@@ -227,64 +227,11 @@ async fn upload_report(content_type: &ContentType, data: Data<'_>) -> String {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .configure(rocket::Config {
+            address: "0.0.0.0".parse().unwrap(),
+            port: 8002,
+            ..rocket::Config::default()
+        })
         .mount("/", routes![get_report_list, get_report_list_post, get_report, upload_report])
         .mount("/", FileServer::from(relative!("static"))) // 指定static目錄
 }
-
-/*use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
-};
-use fsReportRust::ThreadPool;
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8089").unwrap();
-    let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        
-        pool.execute(|| {
-            handle_connection(stream);
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = match &request_line[..] { // [..] 表示取得整個字串的切片。
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "src/hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(10));
-            ("HTTP/1.1 200 OK", "src/hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "src/404.html"),
-    };
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-    let response = format!(
-        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-    );
-    stream.write_all(response.as_bytes()).unwrap();
-}*/
-
-/*
-// old version
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()    // 逐行讀取，並產生一個迭代器, 迭代器會產生 Result<String, io::Error>
-        .map(|result| result.unwrap()) // |result| result.unwrap() 是一個閉包, map是迭代器轉換器，將資料透過閉包轉換
-        .take_while(|line| !line.is_empty()) // take_while為迭代器轉換器，直到發現一個空白行為止
-        .collect(); // 迭代器收集器，將所有元素收集到一個集合中(Vec<String>)。
-
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
-}
-*/
